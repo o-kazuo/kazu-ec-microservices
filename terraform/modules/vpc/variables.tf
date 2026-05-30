8173ed23166c43047f19d08e80066fa3d6582b1e
o-kazuo <itpro.kazu@gmail.com>
2026-05-30 12:37:37 +0900

    feat: add vpc module

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+  description = "プロジェクト名"
+  type        = string
+}
+
+variable "environment" {
+  description = "環境名"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPCのCIDRブロック"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "パブリックサブネットのCIDRリスト"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "プライベートサブネットのCIDRリスト"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "availability_zones" {
+  description = "使用するAZ"
+  type        = list(string)
+  default     = ["ap-northeast-1a", "ap-northeast-1c"]
+}
